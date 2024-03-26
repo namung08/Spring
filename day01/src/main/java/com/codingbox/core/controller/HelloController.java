bae35f00853a726f1837de2b372748383475d123
namung08 <namung08@gmail.com>
2024-03-26 14:31:36 +0900

    feat: add hello-mvc

--- day01/src/main/java/com/codingbox/core/controller/HelloController.java
+++ day01/src/main/java/com/codingbox/core/controller/HelloController.java
@@ -10,7 +10,20 @@ public class HelloController {
     @GetMapping("hello") //주소에서 /hello 로 매핑이 됨
     public String hello(Model model) {
         model.addAttribute("data","firstData");
-        return "hello";
+        return "forward:/hello";
+    }
+
+    /*
+    * url : get 방식
+    *       /hello-mvc
+    * data :    key -> name, value -> name
+    * 화면 : hello-template.html(name 데이터 출력)
+    * 35 분 까지
+    * */
+    @GetMapping("/hello-mvc")
+    public String hellomvc(Model model) {
+        model.addAttribute("name","이상준");
+        return "forward:/hello-mvc";
     }
 
 }
