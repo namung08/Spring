2b4df44db2a3afa7b846e1259b7258d3d5009317
namung08 <namung08@gmail.com>
2024-03-26 16:09:09 +0900

    build: add event controller

--- day01/src/main/java/com/codingbox/core/controller/MemberController.java
+++ day01/src/main/java/com/codingbox/core/controller/MemberController.java
@@ -1,5 +1,16 @@
 package com.codingbox.core.controller;
 
-public class MemberController {
+import com.codingbox.core.dto.MemberDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class MemberController {
+    @RequestMapping("member")
+    public String getMember(Model model) {
+        MemberDTO member = new MemberDTO(1,"java","01012345678");
+        model.addAttribute("member", member);
+        return "/thymeleaf/member";
+    }
 }
