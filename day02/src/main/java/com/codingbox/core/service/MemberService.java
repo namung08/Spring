0b61efa2bfd66fabd7529c9571a47082068fb1ef
namung08 <namung08@gmail.com>
2024-03-27 11:02:09 +0900

    perf: add @service

--- day02/src/main/java/com/codingbox/core/service/MemberService.java
+++ day02/src/main/java/com/codingbox/core/service/MemberService.java
@@ -3,9 +3,11 @@ package com.codingbox.core.service;
 import com.codingbox.core.member.Member;
 import com.codingbox.core.repository.MemberRepository;
 import com.codingbox.core.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MemberService {
     MemberRepository memberRepository = new MemoryMemberRepository();
     // 회원가입
