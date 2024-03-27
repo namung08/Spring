13380376688830e6e7586653261aa4fdb6d5a74a
namung08 <namung08@gmail.com>
2024-03-27 14:16:40 +0900

    docs: dont use Repo

--- day02/src/main/java/com/codingbox/core/repository/MemoryMemberRepository.java
+++ day02/src/main/java/com/codingbox/core/repository/MemoryMemberRepository.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Repository
+//@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     // 메모리를 사용
