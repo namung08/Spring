dd29980fa3d4ba19ec16fd572c832980631d1d70
namung08 <namung08@gmail.com>
2024-03-27 14:49:49 +0900

    refactor: move repo

--- day02/src/main/java/com/codingbox/core/service/MemberService.java
+++ day02/src/main/java/com/codingbox/core/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.codingbox.core.service;
 
 import com.codingbox.core.dto.Member;
+import com.codingbox.core.repository.JdbcMemberRepository;
 import com.codingbox.core.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,21 +11,19 @@ import java.util.List;
 @Service
 public class MemberService {
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    JdbcMemberRepository jdbcMemberRepository;
 
-    MemberRepository memberRepository;
-
-    @Autowired
-    public MemberService(MemberRepository memberRepository){
-        this.memberRepository = memberRepository;
+    public MemberService(JdbcMemberRepository jdbcMemberRepository) {
+        this.jdbcMemberRepository = jdbcMemberRepository;
     }
 
     // 회원가입
     public int join(Member member){
-        memberRepository.save(member);
+        jdbcMemberRepository.save(member);
         return member.getId();
     }
     // 전체 회원 조회
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        return jdbcMemberRepository.findAll();
     }
 }
