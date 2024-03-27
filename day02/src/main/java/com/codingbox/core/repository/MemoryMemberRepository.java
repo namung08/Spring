94b2799d08300477f6682608169bd2cb0e78d47d
namung08 <namung08@gmail.com>
2024-03-27 09:42:45 +0900

    feat: 인터페이스 상속 받음

---
+++ day02/src/main/java/com/codingbox/core/repository/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package com.codingbox.core.repository;
+
+import com.codingbox.core.member.Member;
+
+import java.util.List;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
