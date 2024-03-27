b9b41fb879af279bf2c07e45bd5f39a152608327
namung08 <namung08@gmail.com>
2024-03-27 14:08:52 +0900

    feat: add jdbcrepository
    [add jdbcrepo]

---
+++ day02/src/main/java/com/codingbox/core/repository/JdbcMemberRepository.java
@@ -0,0 +1,18 @@
+package com.codingbox.core.repository;
+
+import com.codingbox.core.dto.Member;
+
+import java.util.List;
+
+public class JdbcMemberRepository implements MemberRepository{
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
