87973a49c2452dc4b163aa75af0c251fadbf5a79
namung08 <namung08@gmail.com>
2024-03-27 17:00:59 +0900

    perf: del @repo

--- day02/src/main/java/com/codingbox/core/repository/JdbcMemberRepository.java
+++ day02/src/main/java/com/codingbox/core/repository/JdbcMemberRepository.java
@@ -1,6 +1,7 @@
 package com.codingbox.core.repository;
 
 import com.codingbox.core.dto.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
@@ -20,6 +21,7 @@ public class JdbcMemberRepository implements MemberRepository{
     private ResultSet rs = null;
     private final DataSource dataSource;
     // 생성자
+    @Autowired
     public JdbcMemberRepository(DataSource dataSource) {
         this.dataSource = dataSource;
     }
