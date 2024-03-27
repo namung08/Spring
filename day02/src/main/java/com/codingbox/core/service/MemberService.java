e561189503e8e4ee1f39eb345f4ca5c5cad8b245
namung08 <namung08@gmail.com>
2024-03-27 10:01:49 +0900

    feat: MemberRepository 객체 선언

---
+++ day02/src/main/java/com/codingbox/core/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.codingbox.core.service;
+
+import com.codingbox.core.repository.MemberRepository;
+import com.codingbox.core.repository.MemoryMemberRepository;
+
+public class MemberService {
+    MemberRepository memberRepository = new MemoryMemberRepository();
+    // 회원가입
+
+    // 전체 회원 조회
+}
