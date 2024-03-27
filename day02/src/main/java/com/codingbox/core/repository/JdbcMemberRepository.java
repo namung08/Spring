1532a8c0bcf9c008d8dadbe9b0c7d252078669cf
namung08 <namung08@gmail.com>
2024-03-27 14:19:32 +0900

    feat: add DataSource

--- day02/src/main/java/com/codingbox/core/repository/JdbcMemberRepository.java
+++ day02/src/main/java/com/codingbox/core/repository/JdbcMemberRepository.java
@@ -3,10 +3,16 @@ package com.codingbox.core.repository;
 import com.codingbox.core.dto.Member;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
 public class JdbcMemberRepository implements MemberRepository{
+    private final DataSource dataSource;
+    // 생성자
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     public Member save(Member member) {
