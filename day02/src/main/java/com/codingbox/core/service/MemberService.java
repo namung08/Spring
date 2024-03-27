1791f854482d561fc09ed6d595adc510f7765211
namung08 <namung08@gmail.com>
2024-03-27 10:05:32 +0900

    feat: 기능 추가
    [회원가입 및 모든 유저의 정보 획득]

--- day02/src/main/java/com/codingbox/core/service/MemberService.java
+++ day02/src/main/java/com/codingbox/core/service/MemberService.java
@@ -1,11 +1,20 @@
 package com.codingbox.core.service;
 
+import com.codingbox.core.member.Member;
 import com.codingbox.core.repository.MemberRepository;
 import com.codingbox.core.repository.MemoryMemberRepository;
+
+import java.util.List;
 
 public class MemberService {
     MemberRepository memberRepository = new MemoryMemberRepository();
     // 회원가입
-
+    public int join(Member member) {
+        memberRepository.save(member);
+        return member.getId();
+    }
     // 전체 회원 조회
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
 }
