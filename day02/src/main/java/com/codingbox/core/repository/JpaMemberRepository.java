c8c1213608ef263daf73a27022e04f5057a04d6c
namung08 <namung08@gmail.com>
2024-03-27 17:01:22 +0900

    perf: add @repo

--- day02/src/main/java/com/codingbox/core/repository/JpaMemberRepository.java
+++ day02/src/main/java/com/codingbox/core/repository/JpaMemberRepository.java
@@ -1,19 +1,29 @@
 package com.codingbox.core.repository;
 
 import com.codingbox.core.dto.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class JpaMemberRepository implements MemberRepository{
+    private final EntityManager em;
+    @Autowired
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
     @Override
     public Member save(Member member) {
-        return null;
+        em.persist(member);
+        return member;
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 }
