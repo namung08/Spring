ecbd23f53ac4032b5fb771dc4853152db21a2d99
namung08 <namung08@gmail.com>
2024-03-28 15:10:27 +0900

    feat: 자고 일어나니 생김

---
+++ day03/src/main/java/com/codingbox/core3/web/basic/BasicController.java
@@ -0,0 +1,21 @@
+package com.codingbox.core3.web.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Spring");
+        return "basic.text-basic";
+    }
+    @GetMapping("/basic/text-unescaped")
+    public String textBasicUnescaped(Model model) {
+        model.addAttribute("data", "Hello Spring");
+        return "basic.text-basic";
+    }
+}
