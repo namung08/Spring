fe6f9c256882850e8792bead26e04cfe35c42710
namung08 <namung08@gmail.com>
2024-03-28 15:22:33 +0900

    feat: setting controller

--- day03/src/main/java/com/codingbox/core3/web/basic/BasicController.java
+++ day03/src/main/java/com/codingbox/core3/web/basic/BasicController.java
@@ -1,9 +1,15 @@
 package com.codingbox.core3.web.basic;
 
+import com.codingbox.core3.web.data.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -16,6 +22,24 @@ public class BasicController {
     @GetMapping("/basic/text-unescaped")
     public String textBasicUnescaped(Model model) {
         model.addAttribute("data", "Hello Spring");
-        return "basic.text-unescaped";
+        return "basic/text-unescaped";
+    }
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User();
+        User userB = new User();
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA",userA);
+        map.put("userB",userB);
+
+        model.addAttribute("user",userA);
+        model.addAttribute("users", list);
+        model.addAttribute("usersmap", map);
+
+        return "basic/variable";
     }
 }
