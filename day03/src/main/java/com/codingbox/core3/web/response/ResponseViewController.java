cd6dda79e7c7267dc5423a2dc27c28d09afa8a72
namung08 <namung08@gmail.com>
2024-03-28 14:35:54 +0900

    feat: ResponseViewController 끝

---
+++ day03/src/main/java/com/codingbox/core3/web/response/ResponseViewController.java
@@ -0,0 +1,27 @@
+package com.codingbox.core3.web.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello").addObject("data","Hello!!!");
+        return mav;
+    }
+
+    /*
+    @ResponseBody가 ㅇ벗으면 response/hello로 뷰 리졸버가 실행되어 뷰를 찾고 렌더링 한다.
+    @ResponseBody가 있으면 뷰 리졸버를 실행하지 않고, http 메시지 바디에[ 직접 response/hello라는 문자가 입력, m
+    * */
+    @ResponseBody
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data","modeldata");
+        return "response/hello";
+    }
+}
