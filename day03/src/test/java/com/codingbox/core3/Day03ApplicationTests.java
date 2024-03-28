e118b82928b9ea0e79e564a0dce7dfd676e9744e
namung08 <namung08@gmail.com>
2024-03-28 09:09:09 +0900

    feat: default setting

---
+++ day03/src/test/java/com/codingbox/core3/Day03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingbox.core3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
