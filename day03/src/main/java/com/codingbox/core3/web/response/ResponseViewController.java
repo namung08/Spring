10f46f462feebad5bfc99b59915513d15f7e0389
namung08 <namung08@gmail.com>
2024-03-28 15:02:44 +0900

    feat: ?

--- day03/src/main/java/com/codingbox/core3/web/response/ResponseViewController.java
+++ day03/src/main/java/com/codingbox/core3/web/response/ResponseViewController.java
@@ -15,7 +15,7 @@ public class ResponseViewController {
     }
 
     /*
-    @ResponseBody가 ㅇ벗으면 response/hello로 뷰 리졸버가 실행되어 뷰를 찾고 렌더링 한다.
+    @ResponseBody가 없으면 response/hello로 뷰 리졸버가 실행되어 뷰를 찾고 렌더링 한다.
     @ResponseBody가 있으면 뷰 리졸버를 실행하지 않고, http 메시지 바디에[ 직접 response/hello라는 문자가 입력, m
     * */
     @ResponseBody
