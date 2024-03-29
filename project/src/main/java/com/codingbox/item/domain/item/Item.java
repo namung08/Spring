301cfca523a090c25cd7f5d88380c85bcc0259f9
namung08 <namung08@gmail.com>
2024-03-29 12:05:32 +0900

    feat: set dto

---
+++ project/src/main/java/com/codingbox/item/domain/item/Item.java
@@ -0,0 +1,49 @@
+package com.codingbox.item.domain.item;
+
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price; // 가격이 미정일 때 null로 처리하기 위해
+    private Integer quantity;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Item() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
