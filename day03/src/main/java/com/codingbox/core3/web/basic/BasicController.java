2c8205cb812b584c9c6f9e228b32e17000d85515
namung08 <namung08@gmail.com>
2024-03-29 09:48:43 +0900

    fix: 실행 안되는 오류 수정

--- day03/src/main/java/com/codingbox/core3/web/basic/BasicController.java
+++ day03/src/main/java/com/codingbox/core3/web/basic/BasicController.java
@@ -60,6 +60,12 @@ public class BasicController {
 		model.addAttribute("data","spring");
 		return "basic/literal";
 	}
+	@GetMapping("operation")
+	public String operation(Model model) {
+		model.addAttribute("nullData",null);
+		model.addAttribute("data","spring");
+		return "basic/operation";
+	}
 }
 
 
