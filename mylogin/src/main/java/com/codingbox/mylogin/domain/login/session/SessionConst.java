6f9bd6896bf0ebe7f60b9b15645d1cab2f4d9dc6
namung08 <namung08@gmail.com>
2024-04-03 09:29:34 +0900

    feat: setting session field name

---
+++ mylogin/src/main/java/com/codingbox/mylogin/domain/login/session/SessionConst.java
@@ -0,0 +1,6 @@
+package com.codingbox.mylogin.domain.login.session;
+
+public class SessionConst {
+    // LOGIN_MEMBER 의 상수만 필요해서 가져다 씀
+    public static final String LOGIN_MEMBER = "loginMember";
+}
