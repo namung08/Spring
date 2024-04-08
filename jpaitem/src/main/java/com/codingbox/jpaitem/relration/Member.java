82a87ce73901107334583252d4cca3fe2a98770b
namung08 <namung08@gmail.com>
2024-04-08 12:29:04 +0900

    feat: 이거 뭐임?

--- jpaitem/src/main/java/com/codingbox/jpaitem/relration/Member.java
+++ jpaitem/src/main/java/com/codingbox/jpaitem/relration/Member.java
@@ -8,11 +8,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter@Setter
 @NoArgsConstructor
 public class Member {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    @SequenceGenerator(name="member_seq_generator", sequenceName = "member_SEQ", allocationSize=1)
     @Column(name = "MEMBER_ID")
     private Long id;
     @Column(name = "USERNAME")
@@ -22,4 +23,38 @@ public class Member {
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMember().add(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", team=" + team +
+                '}';
+    }
 }
