190ce87fc6a3b8855a14d4917cbb75ce9e8b738b
namung08 <namung08@gmail.com>
2024-04-09 11:36:54 +0900

    feat: enum 공부

---
+++ shop/src/main/java/com/codingbox/shop/test/Main01.java
@@ -0,0 +1,9 @@
+package com.codingbox.shop.test;
+
+public class Main01 {
+    public static void main(String[] args) {
+//        Week today = Week.FRIDAY;
+//        System.out.println(today);
+        Week.MONDAY.dayInfo();
+    }
+}
